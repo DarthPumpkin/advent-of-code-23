9dbaa6d4e6cc49c3b3c08464a670b0e9508b1f5f
Dominik Fay <dominik.fay@gmx.de>
2024-02-12 17:49:50 +0100

    Day 14, Part 1

---
+++ day14/src/bin/part1.rs
@@ -0,0 +1,74 @@
+#![feature(test, iter_map_windows)]
+extern crate test;
+
+use std::path::Path;
+
+use day14::{read_input, parse_input, write_output, INPUT_FILE};
+use day14::{PuzzleInput, Column};
+
+
+fn main() {
+    let output = read_and_solve(INPUT_FILE).to_string();
+    write_output(output);
+}
+
+
+fn read_and_solve(file_path: impl AsRef<Path>) -> u64 {
+    let input = read_input(file_path).unwrap();
+    let input = parse_input(&input).unwrap();
+    solve(&input)
+}
+
+fn solve(input: &PuzzleInput) -> u64 {
+    input.columns.iter().map(|col| solve_column(input.height, col)).sum()
+}
+
+fn solve_column(height: usize, column: &Column) -> u64 {
+    let mut total = 0;
+    let (mut cur_cube, mut n_rounds_on_cur_cube) = (0, 0);
+    let mut round_iter = column.round_positions.iter().peekable();
+    let mut cube_iter = column.cube_positions.iter().peekable();
+    while let Some(next_round) = round_iter.peek() {
+        if let Some(next_cube) = cube_iter.peek() {
+            if next_cube < next_round {
+                cur_cube = **next_cube + 1;
+                n_rounds_on_cur_cube = 0;
+                cube_iter.next();
+                continue;
+            }
+        }
+        let height = height as u64;
+        let score = height - cur_cube as u64 - n_rounds_on_cur_cube;
+        total += score;
+        n_rounds_on_cur_cube += 1;
+        round_iter.next();
+    }
+    total
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_SOLUTION: u64 = 136;
+    const TEST_COL_1: u64 = 34;
+    const TEST_COL_2: u64 = 27;
+
+    #[test]
+    fn test_part1() {
+        let test_input = parse_input(&read_input("test_input.txt").unwrap()).unwrap();
+        let output_1 = solve_column(test_input.height, &test_input.columns[0]);
+        let output_2 = solve_column(test_input.height, &test_input.columns[1]);
+        assert_eq!(output_1, TEST_COL_1);
+        assert_eq!(output_2, TEST_COL_2);
+
+        let test_output = read_and_solve("test_input.txt");
+        assert_eq!(test_output, TEST_SOLUTION);
+    }
+
+    #[bench]
+    fn bench_part1(b: &mut test::Bencher) {
+        b.iter(|| read_and_solve(INPUT_FILE));
+    }
+}
---
+++ day14/src/lib.rs
@@ -0,0 +1,104 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::str::FromStr;
+
+pub const INPUT_FILE: &str = "input.txt";
+
+
+pub fn read_input(path: impl AsRef<Path>) -> io::Result<String> {
+    fs::read_to_string(path)
+}
+
+pub fn write_output(output: impl ToString) {
+    println!("{}", output.to_string());
+}
+
+pub fn parse_input(input: &str) -> Result<PuzzleInput, <PuzzleInput as FromStr>::Err> {
+    input.parse()
+}
+
+#[derive(Clone, Debug)]
+pub struct PuzzleInput {
+    pub height: usize,
+    pub columns: Vec<Column>
+}
+
+impl FromStr for PuzzleInput {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<PuzzleInput, Self::Err> {
+        let lines: Vec<_> = s.lines().collect();
+        let (height, width) = (lines.len(), lines[0].len());
+        let mut columns: Vec<_> = (0..width).map(|_| Column { cube_positions: vec![], round_positions: vec![] }).collect();
+        for (i, line) in lines.iter().enumerate() {
+            for (j, c) in line.chars().enumerate() {
+                match Entry::from_char(c) {
+                    Ok(Entry::CUBE) => columns[j].cube_positions.push(i),
+                    Ok(Entry::ROUND) => columns[j].round_positions.push(i),
+                    Ok(Entry::GROUND) => (),
+                    Err(e) => return Err(e)
+                }
+            }
+        }
+        Ok(PuzzleInput { height, columns })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Column {
+    pub cube_positions: Vec<usize>,
+    pub round_positions: Vec<usize>
+}
+
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Entry {
+    CUBE, ROUND, GROUND
+}
+
+impl Entry {
+    pub fn from_char(c: char) -> Result<Entry, String> {
+        match c {
+            '.' => Ok(Entry::GROUND),
+            '#' => Ok(Entry::CUBE),
+            'O' => Ok(Entry::ROUND),
+            _ => Err(format!("Invalid character: {}", c))
+        }
+    }
+}
+
+impl std::fmt::Display for Entry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Entry::GROUND => write!(f, "."),
+            Entry::CUBE => write!(f, "#"),
+            Entry::ROUND => write!(f, "O")
+        }
+    }
+}
+
+
+/// Parse a whitespace-separated list of numbers
+/// 
+/// # Arguments
+/// 
+/// * `numbers` - A string containing a whitespace-separated list of numbers
+/// 
+/// # Returns
+/// 
+/// A vector of parsed numbers
+/// 
+/// # Example
+/// 
+/// ```
+/// let numbers = "1 2 3 4 5";
+/// let parsed = day14::parse_numbers(numbers);
+/// assert_eq!(parsed, Ok(vec![1, 2, 3, 4, 5]));
+/// ```
+pub fn parse_numbers<T: FromStr>(numbers: &str) -> Result<Vec<T>, <T as FromStr>::Err> {
+    numbers
+        .split_whitespace()
+        .map(|x| x.parse())
+        .collect()
+}
