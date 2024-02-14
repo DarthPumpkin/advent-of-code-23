ca5831fc1c411956b0fcd8101781cd4f08b40349
Dominik Fay <dominik.fay@gmx.de>
2024-02-14 01:24:54 +0100

    Make Rust template, restructured

---
+++ template/src/today.rs
@@ -0,0 +1,58 @@
+use std::fmt;
+use std::str::FromStr;
+
+// use crate::aux;
+
+
+pub type Solution1 = u64;
+pub type Solution2 = u64;
+
+
+pub fn solve_part1(input: &PuzzleInput) -> Solution1 {
+    todo!()
+}
+
+pub fn solve_part2(input: &PuzzleInput) -> Solution2 {
+    todo!()
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct PuzzleInput {
+    // ...
+}
+
+impl FromStr for PuzzleInput {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<PuzzleInput, Self::Err> {
+        todo!()
+    }
+}
+
+impl fmt::Display for PuzzleInput {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!()
+    }
+}
+
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+enum Symbol {
+    // ...
+}
+
+impl Symbol {
+    fn from_char(c: char) -> Result<Symbol, String> {
+        match c {
+            _ => Err(format!("Invalid character: {}", c))
+        }
+    }
+}
+
+impl fmt::Display for Symbol {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            _ => todo!()
+        }
+    }
+}
